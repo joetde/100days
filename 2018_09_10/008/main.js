4d54b49b3ea99093d6d42cee28e5434e30d06ac3
Guillaume Berard <guillaume@squareup.com>
2018-09-17 11:48:28 -0400

    Day 008 - mini cleanup

--- 2018_09_10/008/main.js
+++ 2018_09_10/008/main.js
@@ -57,39 +57,6 @@ function forEachPixel(pixel, x, y) {
     return pixel;
 };
 
-function update_position(p) {
-    var sign=1;
-    if (rand_btw(0, 2) == 0) {
-        sign = -1;
-    }
-
-    if (rand_btw(0, 2) == 0) {
-        p.x += sign;
-    } else {
-        p.y += sign;
-    }
-
-    check_point_in_box(p);
-
-    // var c_idx = Math.min(p.custom_color + rand_btw(-50, 50), d.COLOR_WHEEL_MAX);
-    d.grid[p.x][p.y] = p.custom_color;
-    // d.bmd.setPixel(p.x, p.y, d.colors[c_idx].r, d.colors[c_idx].g, d.colors[c_idx].b);
-}
-
-function check_point_in_box(p) {
-    if (p.x > d.game.width) {
-        p.x = d.game.width;
-    } else if (p.x < 0) {
-        p.x = 0;
-    }
-
-    if (p.y > d.game.height) {
-        p.y = d.game.height;
-    } else if (p.y < 0) {
-        p.y = 0;
-    }
-}
-
 function get_image() {
     return d.game.canvas.toDataURL();
 }
